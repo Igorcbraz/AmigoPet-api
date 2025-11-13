8879bc89a0337e0eecf836d6375b1224adc67662
Igor Costa Braz <Igorcbraz1@gmail.com>
2025-11-13 17:31:18 -0300

    chore: cors render

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(cors())
+app.options('*', cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(authenticateToken)
